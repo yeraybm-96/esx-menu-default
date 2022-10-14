33e60cec02adbb42e7c92fd4471e27391e403001
miquelmq20 <43810248+miquelmq20@users.noreply.github.com>
2022-10-14 13:10:46 +0200

    New

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'miquelmq20'
+
+description 'ESX Menu Default by miquelmq20'
+
+client_scripts {
+	'@es_extended/imports.lua',
+	'client/main.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	files {"**/**/**/**/**/**/**/*.*"}
+}
+
+dependencies {
+	'es_extended'
+}
